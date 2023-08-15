cb93ada79f6c9d08ba81d29cbd42ec003d4f9ba7
arya <arya.lavasani@gmail.com>
2023-08-15 23:56:57 +0330

    [feat] add getter methods

--- src/Contracts/DriverMessage.php
+++ src/Contracts/DriverMessage.php
@@ -5,12 +5,17 @@ namespace Aryala7\Chapaar\Contracts;
 interface DriverMessage
 {
     public function setFrom($from);
+    public function getFrom();
 
-    public function setTo($from);
+    public function setTo($to);
+
+    public function getTo();
+
 
     public function setContent(string $content);
 
     public function setTemplate($template);
+    public function getTemplate();
 
     public function setTokens(array $tokens);
 }
